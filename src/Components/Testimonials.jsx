8d1e0ee784510141a07bcb0864828a80ed7ba8b4
Bhimsen <pro.bhimsen@gmail.com>
2023-08-22 22:16:35 +0530

    update tablet responsive styles

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -10,7 +10,7 @@ const Testimonials = () => {
                 <li className="testimonials__list-item">
                     <div className="testmonials__list-item-card">
                         <div className="testimonials__list-item-card-head">
-                            <h2>suraj</h2>
+                            <h2>Suraj</h2>
                         </div>
                         <div className="testimonials__list-item-card-body">
                             <p> have recently tried OV cab and my experience was really good.</p>
@@ -20,7 +20,7 @@ const Testimonials = () => {
                 <li className="testimonials__list-item">
                     <div className="testmonials__list-item-card">
                         <div className="testimonials__list-item-card-head">
-                            <h2>this is that</h2>
+                            <h2>Vaani</h2>
                         </div>
                         <div className="testimonials__list-item-card-body">
                             <p>It was really nice experience, would recommend others to give it a try.</p>
@@ -30,7 +30,7 @@ const Testimonials = () => {
                 <li className="testimonials__list-item">
                     <div className="testmonials__list-item-card">
                         <div className="testimonials__list-item-card-head">
-                            <h2>this is that</h2>
+                            <h2>Aditya</h2>
                         </div>
                         <div className="testimonials__list-item-card-body">
                             <p>I loved there support, a quick and professional support by ov team.</p>
